2922c877180bbeb8e7f5eeda584bab84f09c2b65
stevennemery18-stack <stevennemery18@gmail.com>
2026-07-10 03:13:57 +0300

    Create server.ts

---
+++ api/api/server.ts
@@ -0,0 +1,23 @@
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = parseInt(process.env.PORT || '3000', 10);
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url!, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
